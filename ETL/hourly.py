d3eaf5cfa6d208e57f983f9520cfdb6e040d6f1e
Trevor Bossert <tbossert@extole.com>
2013-04-26 16:37:36 -0700

    Changed hourly ETL to place a dummy row if now data is found for that query, may god save our souls

--- ETL/hourly.py
+++ ETL/hourly.py
@@ -13,6 +13,8 @@ getStartHour = datetime.utcnow() - timedelta(hours = 1)
 endHour = getEndHour.strftime('%Y-%m-%d %H')
 startHour = getStartHour.strftime('%Y-%m-%d %H')
 
+
+
 try:
     with zcon:
         zcur = zcon.cursor()
@@ -22,6 +24,7 @@ try:
         zcur.execute("CREATE TABLE `tmpstats` SELECT * FROM stats WHERE datetime >= " + startHour + " AND datetime < " + endHour)
         zcur.execute("CREATE TABLE `tmpsch` SELECT s.id,s.userid,s.gymid,s.classid,c.service,c.duration,s.price,s.datetime,c.spots,c.featured FROM schedule s INNER JOIN classes c ON s.classid = c.id WHERE datetime >= " + startHour + " AND datetime < " + endHour)
         zcur.execute("CREATE TABLE `tmpchkin` SELECT * FROM checkin WHERE datetime >= " + startHour + " AND datetime < " + endHour)
+        zcur.execute("CREATE TABLE `tmpusers` SELECT male,female,city,state,zipcode FROM users WHERE created >= " + startHour + " AND created < " + endHour)
         print startHour
 
         # Per Gym queries
@@ -29,11 +32,26 @@ try:
 
         rows = zcur.fetchall()
         for row in rows:
-            zcur.execute("INSERT INTO barbell.classhourly (gymid,classid,visits,reservations,duration,amount,datetime) SELECT ts.gymid,ts.classid,(SELECT count(id) FROM tmpchkin WHERE classid = ts.classid) AS visits,count(ts.classid) AS reservations,ts.duration AS duration,SUM(ts.price) AS amount,'" + startHour + "' AS datetime FROM tmpsch ts WHERE ts.gymid = " + str(row[0]) + " GROUP BY classid")
-            zcur.execute("INSERT INTO barbell.gymhourly (gymid,classid,service,visits,reservations,duration,price,amount,datetime) SELECT ts.gymid,ts.classid,ts.service,(SELECT count(id) FROM tmpchkin WHERE classid = ts.classid) AS visits,count(ts.classid) AS reservations,ts.duration AS duration,ts.price,SUM(ts.price) AS amount,'" + startHour + "' AS datetime FROM tmpsch ts WHERE ts.gymid = " + str(row[0]))
+            gymid = str(row[0])
+            zcur.execute("SELECT id FROM classes WHERE gymid = " + str(row[0]))
 
-        zcur.execute("INSERT INTO barbell.repeats (userid,gymid,classid,visits) SELECT userid,gymid,classid,@visits := COUNT(userid) FROM tmpchkin GROUP BY userid,classid ON DUPLICATE KEY UPDATE visits=visits+@visits")
+            crows = zcur.fetchall()
+            for crow in crows:
+                zcur.execute("INSERT INTO barbell.classhourly (gymid,classid,visits,reservations,duration,amount,datetime) SELECT ts.gymid,ts.classid,(SELECT count(id) FROM tmpchkin WHERE classid = ts.classid) AS visits,count(ts.classid) AS reservations,ts.duration AS duration,SUM(ts.price) AS amount,'" + startHour + "' AS datetime FROM tmpsch ts WHERE ts.gymid = " + str(crow[0]) + " GROUP BY classid")
+                if (zcur.rowcount < 1):
+                    zcur.execute("INSERT INTO barbell.classhourly (gymid,classid,visits,reservations,duration,amount,datetime) SELECT " + gymid + "," + str(crow[0]) + ",0,0,0,0,'" + startHour + "'")
+            
+            zcur.execute("INSERT INTO barbell.gymhourly (gymid,visits,reservations,duration,price,amount,datetime) SELECT ts.gymid,(SELECT count(id) FROM tmpchkin WHERE classid = ts.classid) AS visits,count(ts.classid) AS reservations,ts.duration AS duration,ts.price,SUM(ts.price) AS amount,'" + startHour + "' AS datetime FROM tmpsch ts WHERE ts.gymid = " + str(crow[0]))
+            if (zcur.rowcount < 1):
+                zcur.execute("INSERT INTO barbell.gymhourly (gymid,visits,reservations,duration,price,amount,datetime) SELECT " + gymid + ",0,0,0,0,0,'" + startHour + "'")               
+
+        zcur.execute("INSERT INTO barbell.repeats (userid,gymid,classid,visits) SELECT userid,gymid,classid,@visits := COUNT(userid) FROM tmpchkin GROUP BY userid,classid ON DUPLICATE KEY UPDATE visits=visits+@visits")           
         zcur.execute("INSERT INTO barbell.hourly (visits,signups,reservations,amount,datetime) SELECT SUM(tc.visits) AS visits,SUM(tu.signups) AS signups,SUM(ts.reservations) AS reservations,SUM(ts.amount) AS amount,'" + startHour + "' AS datetime FROM tmpchkin tc INNER JOIN tmpsch ts ON tc.userid = ts.userid INNER JOIN tmpusers tu ON ts.userid = tu.id") 
+        if (zcur.rowcount < 1):
+            zcur.execute("INSERT INTO barbell.hourly (visits,signups,reservations,amount,datetime) SELECT 0,0,0,0,'" + startHour + "'")    
+        zcur.execute("INSERT INTO barbell.demographic (male,female,city,state,zipcode,total,datetime) SELECT SUM(IF(sex = 'm', 1, 0)) AS male, SUM(IF(sex = 'f', 1, 0)) AS female,city,state,zipcode,COUNT(zipcode) AS total,'" + startHour + "' AS datetime FROM tmpusers GROUP BY zipcode"))
+        if (zcur.rowcount < 1):
+            zcur.execute("INSERT INTO barbell.demographic (male,female,city,state,zipcode,total,datetime) SELECT 0,0,'','','',0,'" + startHour + "'")            
         
         # Drop Temp Tables
         zcur.execute("DROP TABLE tmpusers")
@@ -41,6 +59,7 @@ try:
         zcur.execute("DROP TABLE tmpstats")
         zcur.execute("DROP TABLE tmpsch")
         zcur.execute("DROP TABLE tmpchkin")
+        zcur.execute("DROP TABLE tmpusers")
 
     zcon.commit()
     zcur.close()
@@ -52,6 +71,8 @@ except mdb.Error, e:
     zcur.execute("DROP TABLE tmpstats")
     zcur.execute("DROP TABLE tmpsch")
     zcur.execute("DROP TABLE tmpchkin")
+    zcur.execute("DROP TABLE tmpusers")
+
     zcon.rollback()
     print "Error %d: %s" % (e.args[0],e.args[1])
     sys.exit(1)
